8e987065cbd51ec7ac451000c460501e296bc3e0
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2026-03-24 19:08:19 +0530

    U

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,4 +1,5 @@
 import { FaBook, FaUsers, FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function AboutSection() {
     return (
@@ -36,9 +37,11 @@ export default function AboutSection() {
                     </div>
 
                     {/* Button */}
-                    <button className="mt-6 px-6 py-3 bg-blue-600 dark:bg-blue-500 text-white rounded-lg hover:bg-blue-700 dark:hover:bg-blue-600 transition font-medium">
-                        Explore Books
-                    </button>
+                    <Link to="/course">
+                        <button className="mt-6 px-6 py-3 bg-blue-600 dark:bg-blue-500 text-white rounded-lg hover:bg-blue-700 dark:hover:bg-blue-600 transition font-medium">
+                            Explore Books
+                        </button>
+                    </Link>
                 </div>
 
                 {/* Image Section */}
