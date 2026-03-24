fb58a0f89514d872731905a8dc74c8a228db8e43
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2026-03-24 20:55:51 +0530

    update

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -44,39 +44,40 @@ function Course() {
     }, [hasMore])
 
     return (
-        <div className='max-w-screen-2xl bg-white dark:bg-slate-900 dark:text-white min-h-screen container mx-auto md:px-20 px-4'>
-            <div className='flex flex-col  items-center text-center mt-16 text-gray-800 dark:text-white'>
-                <h1 className='text-2xl mt-20 md:text-4xl'>
-                    <WelcomeTyping
-                        part1="We're delighted to have you"
-                        part2="Here :)"
-                    />
-                </h1>
-                <p className='mt-6 max-w-2xl'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magnam nesciunt animi molestias quis sint asperiores beatae consequuntur natus, iure, ipsam ullam. Distinctio, necessitatibus? Quos animi ullam perferendis facilis adipisci.
-                </p>
+        <div>
+            <div className='max-w-screen-2xl bg-white dark:bg-slate-900 dark:text-white min-h-screen container mx-auto md:px-20 px-4'>
+                <div className='flex flex-col  items-center text-center mt-16 text-gray-800 dark:text-white'>
+                    <h1 className='text-2xl mt-20 md:text-4xl'>
+                        <WelcomeTyping
+                            part1="We're delighted to have you"
+                            part2="Here :)"
+                        />
+                    </h1>
+                    <p className='mt-6 max-w-2xl'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti magnam nesciunt animi molestias quis sint asperiores beatae consequuntur natus, iure, ipsam ullam. Distinctio, necessitatibus? Quos animi ullam perferendis facilis adipisci.
+                    </p>
 
-                <Link to="/">
-                    <button className='mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300'>
-                        Back
-                    </button>
-                </Link>
+                    <Link to="/">
+                        <button className='mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300'>
+                            Back
+                        </button>
+                    </Link>
 
-                <div className='mt-12 w-full'>
-                    {authUser ? (
-                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6'>
-                            {products.map((item) => (
-                                <MemoCard item={item} key={item.id} />
-                            ))}
-                        </div>
-                    ) : (
-                        <GuestCTA />
-                    )}
+                    <div className='mt-12 w-full'>
+                        {authUser ? (
+                            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6'>
+                                {products.map((item) => (
+                                    <MemoCard item={item} key={item.id} />
+                                ))}
+                            </div>
+                        ) : (
+                            <GuestCTA />
+                        )}
 
 
+                    </div>
                 </div>
             </div>
-
             <Footer />
         </div>
     )
