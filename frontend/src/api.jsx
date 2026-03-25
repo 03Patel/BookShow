f0cadaad75ba37ec809c47298d127d0a5f92baee
Ganesh  ji patel <ganeshjipatel108@gmail.com>
2026-03-25 14:01:49 +0530

    update

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.DEV
-        ? import.meta.env.VITE_LOCAL_API
-        : import.meta.env.VITE_PROD_API,
+    baseURL:
+        import.meta.env.MODE === "development"
+            ? import.meta.env.VITE_LOCAL_API
+            : import.meta.env.VITE_PROD_API,
 });
 
 export default API;
